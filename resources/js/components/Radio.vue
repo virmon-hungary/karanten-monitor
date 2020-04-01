3536c8d591d6001e6a19a99e31fed1fc8e99e835
Csanád Kiss-Máté <traubisoda@gmail.com>
2020-04-01 02:09:45 +0200

    form POC

---
+++ resources/js/components/Radio.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="value in values">
+            <input type="radio" :name="name" :value="value.value" @click="trigger" :checked="value.value == userValue"> {{ value.label }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            values: Array,
+            name: String,
+            userValue: String
+        },
+        methods: {
+            trigger(event) {
+                this.$emit('userInput', event.target.value);
+            }
+        }
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/report', function () {
+    dd(request()->input());
+})->name('report.submit');
+Route::view('/report', 'report', ['form' => "[
+    {
+        type: 'radio',
+        name: 'compareStatus',
+        required: true,
+        title: 'Tegnaphoz képest értékelje saját állapotát:',
+        values: [{label:'Javult', value: 'better'}, {label: 'Nem változott', value: 'unchanged'}, {label: 'Romlott', value: 'worsened'}]
+    },
+    {
+        type: 'checkbox',
+        name: 'symptoms',
+        title: 'Válassza ki az önre igaz tüneteket',
+        values: ['Szárazköhögés', 'Magas láz', 'Torokfájás']
+    },
+    {
+        type: 'textarea',
+        name: 'selfDiagnosis',
+        title: 'Ha szükségét érzi, írja le saját szavaival, hogy érzi magát:'
+    },
+    {
+        type: 'checkbox',
+        title: 'Kötelező választani',
+        name: 'smtg',
+        required: true,
+        values: ['Opció 1', 'Opció 2', 'Opció 3']
+    }
+]"]);
