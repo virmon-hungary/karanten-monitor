9f6023c8f6856b54b52209eb1141bca9925f9267
Berci <stellar124@gmail.com>
2020-04-01 11:15:31 +0200

    controller patient namespace

--- app/Http/Controllers/Patient/Auth/RegisterController.php
+++ app/Http/Controllers/Patient/Auth/RegisterController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\AuthPatient;
+namespace App\Http\Controllers\Patient\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Patient;
--- routes/web.php
+++ routes/web.php
@@ -22,26 +22,26 @@ Auth::routes();
 
 Route::group(['prefix' => 'patient', 'as' => 'patient.'], function () {
 
-    Route::get('login', [\App\Http\Controllers\AuthPatient\LoginController::class, 'showLoginForm'])->name('loginPatient');
-    Route::post('login', [\App\Http\Controllers\AuthPatient\LoginController::class, 'login'])->name('login');
+    Route::get('login', [\App\Http\Controllers\Patient\Auth\LoginController::class, 'showLoginForm'])->name('loginPatient');
+    Route::post('login', [\App\Http\Controllers\Patient\Auth\LoginController::class, 'login'])->name('login');
 
-    Route::get('register', [\App\Http\Controllers\AuthPatient\RegisterController::class, 'showRegistrationForm'])->name('register');
-    Route::post('register', [\App\Http\Controllers\AuthPatient\RegisterController::class, 'register']);
+    Route::get('register', [\App\Http\Controllers\Patient\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [\App\Http\Controllers\Patient\Auth\RegisterController::class, 'register']);
 
-    Route::get('password/reset', [\App\Http\Controllers\AuthPatient\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-    Route::post('password/email', [\App\Http\Controllers\AuthPatient\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-    Route::get('password/reset/{token}', [\App\Http\Controllers\AuthPatient\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-    Route::post('password/reset', [\App\Http\Controllers\AuthPatient\ResetPasswordController::class, 'reset'])->name('password.update');
+    Route::get('password/reset', [\App\Http\Controllers\Patient\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('password/email', [\App\Http\Controllers\Patient\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('password/reset/{token}', [\App\Http\Controllers\Patient\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('password/reset', [\App\Http\Controllers\Patient\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
 
-    Route::get('password/confirm', [\App\Http\Controllers\AuthPatient\ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
-    Route::post('password/confirm', [\App\Http\Controllers\AuthPatient\ConfirmPasswordController::class, 'confirm']);
+    Route::get('password/confirm', [\App\Http\Controllers\Patient\Auth\ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+    Route::post('password/confirm', [\App\Http\Controllers\Patient\Auth\ConfirmPasswordController::class, 'confirm']);
 
-    Route::get('email/verify', [\App\Http\Controllers\AuthPatient\VerificationController::class, 'show'])->name('verification.notice');
-    Route::get('email/verify/{id}/{hash}', [\App\Http\Controllers\AuthPatient\VerificationController::class, 'verify'])->name('verification.verify');
-    Route::post('email/resend', [\App\Http\Controllers\AuthPatient\VerificationController::class, 'resend'])->name('verification.resend');
+    Route::get('email/verify', [\App\Http\Controllers\Patient\Auth\VerificationController::class, 'show'])->name('verification.notice');
+    Route::get('email/verify/{id}/{hash}', [\App\Http\Controllers\Patient\Auth\VerificationController::class, 'verify'])->name('verification.verify');
+    Route::post('email/resend', [\App\Http\Controllers\Patient\Auth\VerificationController::class, 'resend'])->name('verification.resend');
 
     Route::group(['middleware' => ['auth:patient']], function () {
-        Route::get('logout', [\App\Http\Controllers\AuthPatient\LoginController::class,'logout'])->name('logout');
+        Route::get('logout', [\App\Http\Controllers\Patient\Auth\LoginController::class,'logout'])->name('logout');
     });
 });
 
