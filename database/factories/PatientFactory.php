1182dda29d1828d3e7aa529acd154b2ae3fc903b
Berci <stellar124@gmail.com>
2020-04-01 07:56:49 +0200

    multiauth backend kb kesz

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Patient;
+use \Illuminate\Support\Str;
+use \Faker\Provider\Base;
+use Faker\Generator as Faker;
+
+$factory->define(Patient::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+        'taj' => Base::numberBetween(121212121,989898989),
+        'telephone' => '+36' . Base::numberBetween(12121212,70989989)
+    ];
+});
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::group(['prefix' => 'patient'], function () {
+
+    Route::get('login', [\App\Http\Controllers\AuthPatient\LoginController::class, 'showLoginForm'])->name('loginPatient');
+    Route::post('login', [\App\Http\Controllers\AuthPatient\LoginController::class, 'login'])->name('login');
+
+    Route::get('register', [\App\Http\Controllers\AuthPatient\RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [\App\Http\Controllers\AuthPatient\RegisterController::class, 'register']);
+
+    Route::get('password/reset', [\App\Http\Controllers\AuthPatient\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('password/email', [\App\Http\Controllers\AuthPatient\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('password/reset/{token}', [\App\Http\Controllers\AuthPatient\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('password/reset', [\App\Http\Controllers\AuthPatient\ResetPasswordController::class, 'reset'])->name('password.update');
+
+    Route::get('password/confirm', [\App\Http\Controllers\AuthPatient\ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+    Route::post('password/confirm', [\App\Http\Controllers\AuthPatient\ConfirmPasswordController::class, 'confirm']);
+
+    Route::get('email/verify', [\App\Http\Controllers\AuthPatient\VerificationController::class, 'show'])->name('verification.notice');
+    Route::get('email/verify/{id}/{hash}', [\App\Http\Controllers\AuthPatient\VerificationController::class, 'verify'])->name('verification.verify');
+    Route::post('email/resend', [\App\Http\Controllers\AuthPatient\VerificationController::class, 'resend'])->name('verification.resend');
+
+    Route::group(['middleware' => ['auth:patient']], function () {
+        Route::get('logout', [\App\Http\Controllers\AuthPatient\LoginController::class,'logout'])->name('logout');
+    });
+});
+
+
+Route::get('/home', 'HomeController@index')->name('home');
